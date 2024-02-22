3c93176f01294f75a416979f3e178cec0d4db512
molanp <molanp>
2024-02-22 13:55:43 +0800

    修正throw与pick

--- nonebot_zhenxun_plugin_bottle/__init__.py
+++ nonebot_zhenxun_plugin_bottle/__init__.py
@@ -7,6 +7,7 @@ from configs.config import NICKNAME
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11.permission import GROUP
 from nonebot.params import CommandArg
+from models.group_member_info import GroupInfoUser
 from ._model import Sea
 
 
@@ -31,7 +32,7 @@ __plugin_des__ = '漂流瓶插件'
 __plugin_type__ = ("群内小游戏",)
 __plugin_cmd__ = ["pick", "throw", "查看漂流瓶", "扔漂流瓶",
                   "捡漂流瓶", "寄漂流瓶", "丢漂流瓶", "漂流瓶列表", "删除漂流瓶", "清空漂流瓶",
-                  "漂流瓶黑名单", "移出漂流瓶黑名单", "查看漂流瓶黑名单",
+                  "添加漂流瓶黑名单", "移出漂流瓶黑名单", "漂流瓶黑名单",
                   "清空漂流瓶黑名单", "banbottle", "漂流瓶封禁"]
 __plugin_version__ = 0.1
 __plugin_author__ = "molanp"
@@ -52,14 +53,14 @@ __plugin_cd_limit__ = {
 
 throw = on_command("扔漂流瓶", aliases={
                    "寄漂流瓶", "丢漂流瓶", "throw"}, permission=GROUP, priority=5, block=True)
-pick = on_command("捡漂流瓶", aliases={"pick"}, priority=5, block=True)
+pick = on_command("捡漂流瓶", aliases={"pick"}, permission=GROUP,priority=5, block=True)
 check_bottle = on_command(
     "查看漂流瓶", permission=SUPERUSER, priority=5, block=True)
 remove = on_command("删除漂流瓶", permission=SUPERUSER, priority=5, block=True)
 
 clear = on_command("清空漂流瓶", permission=SUPERUSER, priority=5, block=True)
 ban = on_command(
-    "漂流瓶黑名单",
+    "添加漂流瓶黑名单",
     aliases={"banbottle", "漂流瓶封禁"},
     permission=SUPERUSER,
     priority=5,
@@ -67,7 +68,7 @@ ban = on_command(
 )
 clear_ban = on_command("清空漂流瓶黑名单", permission=SUPERUSER,
                        priority=5, block=True)
-check_ban = on_command("查看漂流瓶黑名单", permission=SUPERUSER,
+check_ban = on_command("漂流瓶黑名单", permission=SUPERUSER,
                        priority=5, block=True)
 remove_ban = on_command(
     "移出漂流瓶黑名单", permission=SUPERUSER, priority=5, block=True)
@@ -76,18 +77,29 @@ remove_ban = on_command(
 @throw.handle()
 async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     msg = args.extract_plain_text()
-    await throw.finish(Message(Sea(event).throw(msg)), at_sender=True)
+    name = await get_name(event.user_id, event.group_id)
+    await throw.finish(Message(Sea(event).throw(msg, name)), at_sender=True)
+
+
+async def get_name(user_id: str, group_id: str) -> str:
+    if user := await GroupInfoUser.get_or_none(user_id=user_id, group_id=group_id):
+        return user.user_name
+    else:
+        return "该群员不在列表中，请更新群成员信息"
 
 
 @pick.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
     bottle = Sea(event).pick()
     if bottle:
-        id_str = str(bottle[0])
-        qq_str = str(bottle[2])
-        grp_str = str(bottle[3])
+        try:
+            id_str = bottle.id
+            qq_str = bottle.user
+            grp_str = bottle.grp
+        except BaseException:
+            await pick.finish(Message(bottle), at_sender=True)
         msg_list = []
-        msg = f"{NICKNAME}试着帮你捞出来了这个~\nID: {id_str}\n投递人: {bottle[1]}({qq_str})\n群号: {grp_str}\n时间: {bottle[5]}\n内容: \n{bottle[4]}"
+        msg = f"{NICKNAME}试着帮你捞出来了这个~\nID: {id_str}\n投递人: {bottle.name}({qq_str})\n群号: {grp_str}\n时间: {bottle.time}\n内容: \n{bottle.msg}"
         msg_list.append({
             "type": "node",
             "data": {
@@ -101,6 +113,8 @@ async def _(bot: Bot, event: GroupMessageEvent):
         else:
             for msg in msg_list:
                 await bot.send_private_msg(user_id=event.user_id, message=msg)
+    else:
+        await pick.finish(Message("没有漂流瓶喵~"), at_sender=True)
 
 
 @check_bottle.handle()
@@ -108,11 +122,14 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     id = args.extract_plain_text()
     bottle = Sea(event).pick(id)
     if bottle:
-        id_str = str(bottle[0])
-        qq_str = str(bottle[2])
-        grp_str = str(bottle[3])
+        try:
+            id_str = bottle.id
+            qq_str = bottle.user
+            grp_str = bottle.grp
+        except BaseException:
+            await pick.finish(Message(bottle), at_sender=True)
         msg_list = []
-        msg = f"{NICKNAME}试着帮你捞出来了这个~\nID: {id_str}\n投递人: {bottle[1]}({qq_str})\n群号: {grp_str}\n时间: {bottle[5]}\n内容: \n{bottle[4]}"
+        msg = f"{NICKNAME}试着帮你捞出来了这个~\nID: {id_str}\n投递人: {bottle.name}({qq_str})\n群号: {grp_str}\n时间: {bottle.time}\n内容: \n{bottle.msg}"
         msg_list.append({
             "type": "node",
             "data": {
--- nonebot_zhenxun_plugin_bottle/_model.py
+++ nonebot_zhenxun_plugin_bottle/_model.py
@@ -1,13 +1,26 @@
 import os
 import sqlite3
+import random
+import traceback
+import sys
+from services.log import logger
 from .curse import words
+
+
+class Bottle:
+    def __init__(self, row):
+        self.id = row[0]
+        self.name = row[1]
+        self.user = row[2]
+        self.grp = row[3]
+        self.msg = row[4]
+        self.time = row[5]
 
 
 class Sea:
     def __init__(self, event):
         self.usr = event.user_id
         self.grp = getattr(event, "group_id", None)
-        self.name = event.sender.card
         self.db = sqlite3.connect(os.path.dirname(
             __file__) + "/bottle.db", check_same_thread=False)
         self.cursor = self.db.cursor()
@@ -18,14 +31,13 @@ class Sea:
             "CREATE TABLE IF NOT EXISTS ban (id INTEGER PRIMARY KEY AUTOINCREMENT, uin TEXT, type TEXT, time TEXT);")
         self.db.commit()
 
-
     def contains_prohibited_words(self, text):
         for word in words:
             if word in text:
                 return True
         return False
 
-    def throw(self, msg):
+    def throw(self, msg, name):
         if not self.is_ban():
             if not msg or msg == "" or len(msg) < 10:
                 return "需要投递的内容过少"
@@ -35,49 +47,74 @@ class Sea:
                 return "发现违禁词，请更换投递内容"
             else:
                 try:
-                    self.db.execute(
-                        "INSERT INTO bottles (name, user, _group, msg, time) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP);", (self.name, self.usr, self.grp, msg))
+                    query = "INSERT INTO bottles (name, user, _group, msg, time) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP);"
+                    values = (name, self.usr, self.grp, msg)
+                    self.db.execute(query, values)
                     self.db.commit()
-                except Exception as e:
-                    return "发生错误：{}".format(str(e))
-                finally:
                     return "已经帮你丢出去了哦~"
+                except BaseException as e:
+                    error_type = type(e).__name__
+                    error_message = str(e)
+                    error_traceback = traceback.extract_tb(
+                        sys.exc_info()[2])[-2]
+
+                    result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+                    return result
 
     def pick(self, id=None):
         if not self.is_ban():
             try:
-                if not id or not str(id).isdigit():
-                    sql = "SELECT * FROM bottles ORDER BY RANDOM() LIMIT 1;"
+                if id is None or not str(id).isdigit():
+                    sql = "SELECT * FROM bottles ORDER BY RANDOM();"
                     self.cursor.execute(sql)
                 else:
-                    sql = "SELECT * FROM bottles WHERE id = ? LIMIT 1;"
+                    sql = "SELECT * FROM bottles WHERE id = ?;"
                     self.cursor.execute(sql, (id,))
-                self.db.commit()
+                    
                 row = self.cursor.fetchone()
-                return row if row else None
-            except Exception as e:
-                return str(e)
+                if row is not None:
+                    return Bottle(row)
+                else:
+                    return False
+            except BaseException as e:
+                error_type = type(e).__name__
+                error_message = str(e)
+                error_traceback = traceback.extract_tb(sys.exc_info()[2])[-2]
+
+                result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+                return result
+
 
     def remove(self, id):
         try:
             self.cursor.execute("DELETE FROM bottles WHERE id = ?;", (id,))
             self.db.commit()
-        except Exception as e:
-            return "发生错误：{}".format(str(e))
-        finally:
             return "成功删除id为 {} 的漂流瓶".format(id)
+        except BaseException as e:
+            error_type = type(e).__name__
+            error_message = str(e)
+            error_traceback = traceback.extract_tb(
+                sys.exc_info()[2])[-2]
+
+            result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+            return result
 
     def clear(self):
         try:
             self.cursor.execute("DELETE FROM bottles;")
             self.db.commit()
             count = self.cursor.rowcount
-        except Exception as e:
-            return "发生错误：{}".format(str(e))
-        finally:
             count = count if "count" in locals() else 0
             return "已删除 {} 个漂流瓶".format(count)
-        
+        except BaseException as e:
+            error_type = type(e).__name__
+            error_message = str(e)
+            error_traceback = traceback.extract_tb(
+                sys.exc_info()[2])[-2]
+
+            result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+            return result
+
     def ban(self, msg):
         _type, uin = msg.split()
         if _type == "group":
@@ -98,24 +135,35 @@ class Sea:
                         "INSERT INTO ban (uin, type, time) VALUES (?, ?, CURRENT_TIMESTAMP);", (uin, _type))
                     self.db.commit()
                     return "已将用户({})拉入黑名单".format(uin)
-                except Exception as e:
-                    return "发生错误：{}".format(str(e))
+                except BaseException as e:
+                    error_type = type(e).__name__
+                    error_message = str(e)
+                    error_traceback = traceback.extract_tb(
+                        sys.exc_info()[2])[-2]
+
+                    result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+                    return result
             else:
                 return "该用户已在黑名单内"
         else:
             return "未知操作"
-    
+
     def clear_ban(self):
         try:
             self.cursor.execute("DELETE FROM ban;")
             self.db.commit()
             count = self.cursor.rowcount
-        except Exception as e:
-            return "发生错误：{}".format(str(e))
-        finally:
             count = count if "count" in locals() else 0
             return "已移除 {} 个黑名单记录".format(count)
-        
+        except BaseException as e:
+            error_type = type(e).__name__
+            error_message = str(e)
+            error_traceback = traceback.extract_tb(
+                sys.exc_info()[2])[-2]
+
+            result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+            return result
+
     def ban_list(self):
         try:
             self.cursor.execute("SELECT uin, type, time FROM ban;")
@@ -128,9 +176,15 @@ class Sea:
                     ban_str = "{}({})[{}]".format(row[0], row[1], row[2])
                     bans.append(ban_str)
                 return "当前黑名单列表：{}".format(",\n".join(bans))
-        except Exception as e:
-            return "发生错误：{}".format(str(e))
-    
+        except BaseException as e:
+            error_type = type(e).__name__
+            error_message = str(e)
+            error_traceback = traceback.extract_tb(
+                sys.exc_info()[2])[-2]
+
+            result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+            return result
+
     def remove_ban(self, msg):
         _type, uin = msg.split()
         if _type == "group":
@@ -140,8 +194,14 @@ class Sea:
                         "DELETE FROM ban WHERE uin = ? and type = ?;", (uin, _type))
                     self.db.commit()
                     return "已将群聊:{} 移出黑名单".format(uin)
-                except Exception as e:
-                    return "发生错误：{}".format(str(e))
+                except BaseException as e:
+                    error_type = type(e).__name__
+                    error_message = str(e)
+                    error_traceback = traceback.extract_tb(
+                        sys.exc_info()[2])[-2]
+
+                    result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+                    return result
             else:
                 return "该群不在黑名单内"
         elif _type == "user":
@@ -151,8 +211,14 @@ class Sea:
                         "DELETE FROM ban WHERE uin = ? and type = ?;", (uin, _type))
                     self.db.commit()
                     return "已将用户({})移出黑名单".format(uin)
-                except Exception as e:
-                    return "发生错误：{}".format(str(e))
+                except BaseException as e:
+                    error_type = type(e).__name__
+                    error_message = str(e)
+                    error_traceback = traceback.extract_tb(
+                        sys.exc_info()[2])[-2]
+
+                    result = f'ERROR:\nType: {error_type}\nMessage: {error_message}\nLine: {error_traceback.lineno}\nFile: {error_traceback.filename}\nFunction: {error_traceback.name}'
+                    return result
             else:
                 return "该用户不在黑名单内"
         else:
@@ -168,6 +234,6 @@ class Sea:
         self.cursor.execute(
             "SELECT COUNT(*) FROM ban WHERE uin=? and type=?;", (uin, _type))
         result = self.cursor.fetchone()
-        if result[0] > 0:
+        if result[0] > 0 and result is not None:
             return True
         return False
